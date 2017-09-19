711f97acfe8454b9473aae114df53674679cf971
Antman261 <anthony@gigger.rocks>
2017-09-19 21:46:48 +0800

    Grunt file now cleans old production assets from destination.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,31 @@
 module.exports = function (grunt) {
-  var configPath = grunt.option('config') || 'django_deploy_config.json';
-  var config = grunt.file.readJSON(configPath);
-  var assetData = grunt.file.readJSON(config.assetManifest);
+  let configPath = grunt.option('config') || 'django_deploy_config.json';
+  let config = grunt.file.readJSON(configPath);
+  let assetData = grunt.file.readJSON(config.assetManifest);
+  let assetDataToClean = grunt.file.readJSON(config.assetManifest);
   grunt.log.write('Reading from', configPath, '...\n');
   grunt.log.write('Config is', config, '\n');
   grunt.log.write('Asset data is', assetData, '\n');
-  for (var k in assetData) {
+
+  let cleanArr = [];
+  for (let k in assetData) {
     if (assetData.hasOwnProperty(k)) {
+      let trimmedAsset = assetData[k].replace('static/', '');
+      let assetArr = trimmedAsset.split('.');
+      if (assetArr.length === 3) {
+        cleanArr.push(`${config.staticDest}${assetArr[0]}.*.${assetArr[2]}`);
+      } else {
+        cleanArr.push(`${config.staticDest}${assetArr[0]}.*.${assetArr[2]}.${assetArr[3]}`);
+      }
+
+      cleanArr.push(`!${config.staticDest}${trimmedAsset}`);
       let newKey = k.replace('.', '_');
-      assetData[newKey] = assetData[k].replace('static/', '');
+      assetData[newKey] = trimmedAsset;
       delete assetData[k];
     }
   }
+
+  grunt.log.write('Cleaning arr:\n', cleanArr, '\n');
 
   grunt.initConfig({
     mustache_render: {
@@ -32,10 +46,17 @@ module.exports = function (grunt) {
         src: '**',
         dest: config.staticDest
       }
+    },
+    clean: {
+      options: {
+        force: true
+      },
+      deploy: cleanArr
     }
   });
   grunt.loadNpmTasks('grunt-mustache-render');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['copy', 'mustache_render']);
+  grunt.registerTask('default', ['copy', 'mustache_render', 'clean']);
 }
